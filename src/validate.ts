dd8db4db777b1ef47c087f187df267dd234f0786
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-13 15:12:05 -0400

    Add case & empty line validation

--- src/validate.ts
+++ src/validate.ts
@@ -2,6 +2,12 @@ import path from 'path';
 import { classToSlug, fetchClasses, parseMarkers, secondsToDHMS } from './search';
 import { getMissingStrings } from './shared';
 
+const getWordBefore = (string: string, ...args: Parameters<typeof String.prototype.split>) =>
+  string
+    .split(...args)[0]
+    .split(/\s+/)
+    .slice(-1)[0];
+
 fetchClasses().then(async classes => {
   console.log(getMissingStrings(classes).join('\n'));
 
@@ -15,27 +21,37 @@ fetchClasses().then(async classes => {
     const entries = Array.from((await parseMarkers(path.join(info.absolute, 'markers')))?.entries()!);
     const places = secondsToDHMS(entries[entries.length - 1][0]).split(':').length;
 
+    let empty = false;
     let last = null;
     for (const [seconds, marker] of entries) {
-      if (!marker) continue;
+      if (!marker) {
+        empty = true;
+        continue;
+      }
+
       const lowMarker = marker.toLowerCase();
       if (lowMarker.endsWith(' started')) {
         if (last !== null) {
           log(secondsToDHMS(seconds, places) + '\t' + 'Expected current to end before starting another');
         }
-        last = lowMarker.split(' started')[0].trim();
+        if (!marker.endsWith(' Started')) {
+          log(secondsToDHMS(seconds, places) + '\t' + 'Expected Started to be capitalized');
+        }
+        last = getWordBefore(marker, / started/i);
       }
       if (lowMarker.endsWith(' ended')) {
         if (last === null) {
           log(secondsToDHMS(seconds, places) + '\t' + 'Expected start before an end');
-        } else if (last !== lowMarker.split(' ended')[0].trim()) {
+        } else if (last !== getWordBefore(marker, / ended/i)) {
           log(secondsToDHMS(seconds, places) + '\t' + 'Expected end to match start');
+        }
+        if (!marker.endsWith(' Ended')) {
+          log(secondsToDHMS(seconds, places) + '\t' + 'Expected Ended to be capitalized');
         }
         last = null;
       }
     }
-    if (last) {
-      log('Never ended');
-    }
+    if (last) log(`${last} never ended`);
+    if (empty) log('Blank line within');
   }
 });
